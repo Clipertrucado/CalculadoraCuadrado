878b4bde91dc2ae6f772f8a863999ca01fc62bc3
DESKTOP-C638FBT\csi23-salflet <csi23-salflet@colegioaltair.net>
2023-10-24 12:04:22 +0200

    Agregar archivos de proyecto.

---
+++ Controladores/Program.cs
@@ -0,0 +1,55 @@
+﻿using CalcularCuadrado.Servicios;
+using System.ComponentModel;
+
+namespace CalcularCuadradro;
+
+class Program
+{
+    static void Main(string[] args) 
+    {
+        int n1;
+        bool cerrar = false;
+
+        while (!cerrar)
+        {
+            MenuInterfaz mi = new MenuImplementacion();
+
+            int selec = mi.Menu();
+
+            switch (selec)
+            {
+                case 0:
+                    Console.WriteLine("[INFO] - se ejecuta caso uno 0");
+                    cerrar = true;
+                break;
+
+                case 1:
+                    Console.WriteLine("Ingresa un número");
+                    n1 = Convert.ToInt32(Console.ReadLine());
+                    double raiz = Math.Sqrt(n1);
+                    int raizChica = Convert.ToInt32(raiz);
+                     Console.WriteLine(raiz);
+
+                    double resul = raiz - raizChica;
+
+                    if (resul == 0)
+                    {
+                        Console.WriteLine("Tiene raiz entera: "+raiz);
+                    }
+                    else
+                    {
+                        Console.WriteLine("No tiene raiz entera, su raiz es: " + raiz);
+                    }
+
+                break;
+
+                default:
+                    Console.WriteLine("");
+                break;
+
+            }
+
+        }
+    }
+}
+
---
+++ Servicios/MenuImplementacion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularCuadrado.Servicios
+{
+    internal class MenuImplementacion : MenuInterfaz
+    {
+        public int Menu()
+        {
+            int selec;
+
+            Console.WriteLine("###########################");
+            Console.WriteLine("0. Se cerrara la aplicacion");
+            Console.WriteLine("1. Introducir un número");
+            Console.WriteLine("###########################");
+            selec = Convert.ToInt32(Console.ReadLine());
+
+            return selec;
+        }
+    }
+}
